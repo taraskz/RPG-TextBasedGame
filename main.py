5aac69c22f40371c3208fd14780531a2f6eb202f
taraskz <158589585+taraskz@users.noreply.github.com>
2024-03-13 15:20:29 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+##############################################################################
+# Title: Marvel Superheroes - Part 4
+# Class: CS 30
+# Date: February 27, 2024
+# Coders: Taras K
+# Version: 004
+##############################################################################
+'''This is a game menu using a dictionary of marvel superheros
+   the player will get to pick his character and weapon to complete the game
+   '''
+##############################################################################
